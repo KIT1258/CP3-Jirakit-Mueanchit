65e55ad0f57b93eaf02ab892060bf45c1e09429a
KIT1258 <78857287+KIT1258@users.noreply.github.com>
2021-04-07 17:55:32 +0700

    Update Exercise4_Jirakit Mueanchit.py

--- Exercise4_Jirakit Mueanchit.py
+++ Exercise4_Jirakit Mueanchit.py
@@ -1,1 +1,10 @@
-
+FoundationEng = 68.5
+Generalbusiness = 69.5
+Introductioncomsystem = 74.5
+ComProgram  = 79.5
+print("----Your Score----")
+print("Foundation English :",FoundationEng)
+print("General Business :",Generalbusiness)
+print("Introduction to Computer Systems :",Introductioncomsystem)
+print("Computer Programming :",ComProgram)
+print("----Thankyou-----")
